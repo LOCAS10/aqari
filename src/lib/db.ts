18c0c5aeccfb08d767c66ba7b1c8dcf8f26745fe
Z User <z@container>
2026-07-07 23:43:03 +0000

    إصلاح PrismaLibSql export

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,26 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
+function createPrismaClient() {
+  const url = process.env.DATABASE_URL!
+  const authToken = process.env.DATABASE_AUTH_TOKEN
+
+  if (url.startsWith('libsql://') && authToken) {
+    const libsql = createClient({ url, authToken })
+    const adapter = new PrismaLibSql(libsql)
+    return new PrismaClient({ adapter })
+  }
+
+  return new PrismaClient({
+    log: process.env.NODE_ENV !== 'production' ? ['query'] : [],
   })
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
